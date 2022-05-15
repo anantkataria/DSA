c1267b58b0149ffa580d775c133d91c610ef79da
Anant Kataria <katariaanant4@gmail.com>
2022-05-15 12:27:47 +0530

    improvements in find_peak_element_from_array.cpp code

--- find_peak_element_from_array.cpp
+++ find_peak_element_from_array.cpp
@@ -15,77 +15,62 @@ class Solution
     int peakElement(int arr[], int n)
     {
        // Your code here
-       int curri;
-       int previ;
-       int nexti;
-       
-       //I forgot the edge case when there is only one element
-       if(n == 1){
+       if(n==1){
            return 0;
        }
-       
-       for( curri=0; curri<n; curri++){
-           previ = curri-1;
-           nexti = curri+1;
-           if(previ >= 0 && nexti < n){
-               if(arr[curri]>=arr[previ] && arr[curri]>=arr[nexti]){
-                   return curri;
-               }
-           }
-           else if (previ >= 0){
-               if(arr[curri]>=arr[previ]){
-                   return curri;
-               }
-           }
-           else {
-               if(arr[curri]>=arr[nexti]){
-                   return curri;
-               }
-           }
+       if(arr[0] >= arr[1]){
+           return 0;
+       }
+       if(arr[n-1]>=arr[n-2]){
+           return n-1;
        }
        
-       return curri;
+       for(int i=1; i<n-1; i++){
+           if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1]){
+               return i;
+           }
+       }
     }
 };
 
 // { Driver Code Starts.
 
 int main() {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		int a[n], tmp[n];
-		for(int i=0;i<n;i++)
-		{
-			cin>>a[i];
-			tmp[i] = a[i];
-		}
-		bool f=0;
-		Solution ob;
-		
-		int A = ob. peakElement(tmp,n);
-		
-		if(A<0 and A>=n)
-		    cout<<0<<endl;
-		else
-		{
-    		if(n==1 and A==0)
-    		    f=1;
-    		else if(A==0 and a[0]>=a[1])
-    		    f=1;
-    		else if(A==n-1 and a[n-1]>=a[n-2])
-    		    f=1;
-    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
-    		    f=1;
-    		else
-    		    f=0;
-    		cout<<f<<endl;
-		}
-		
-	}
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n], tmp[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            tmp[i] = a[i];
+        }
+        bool f=0;
+        Solution ob;
+        
+        int A = ob. peakElement(tmp,n);
+        
+        if(A<0 and A>=n)
+            cout<<0<<endl;
+        else
+        {
+            if(n==1 and A==0)
+                f=1;
+            else if(A==0 and a[0]>=a[1])
+                f=1;
+            else if(A==n-1 and a[n-1]>=a[n-2])
+                f=1;
+            else if(a[A]>=a[A+1] and a[A]>= a[A-1])
+                f=1;
+            else
+                f=0;
+            cout<<f<<endl;
+        }
+        
+    }
 
-	return 0;
+    return 0;
 }  // } Driver Code Ends
