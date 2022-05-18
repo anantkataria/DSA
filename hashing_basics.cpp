7b42d237ccf02eb53e222c4f41a56734ed645c19
Anant Kataria <katariaanant4@gmail.com>
2022-05-18 12:35:19 +0530

    Learn hashing basics

---
+++ hashing_basics.cpp
@@ -0,0 +1,14 @@
+//learnings about hashing
+
+//hashing is an improvement over direct access table
+
+//hash function - converts given key to a small practical integer value
+// this integer value is used as an index in hash table
+
+//collision handling - situation where newly inserted key maps to an already occupied slot in hash table is called collision.
+//1) Chaining
+//2) Open Addressing
+
+//index mapping is a trivial hashing
+
+	
