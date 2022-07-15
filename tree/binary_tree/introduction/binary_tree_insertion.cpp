c7afe3d8eec4601f124dcf130ca8f8c9eef99a1f
Anant Kataria <katariaanant4@gmail.com>
2022-07-15 08:26:19 +0530

    add binary tree section

---
+++ tree/binary_tree/introduction/binary_tree_insertion.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Btree {
+public:
+	int data;
+	Btree* left;
+	Btree* right;
+
+	Btree(int x){
+		data = x;
+		left = right = NULL;
+	}
+	Btree* getLeft() {
+		return left;
+	}
+	Btree* getRight() {
+		return right;
+	}
+};
+
+Btree* getNewNode(int x){
+	Btree *newNode = new Btree(x);
+	return newNode;
+}
+
+Btree* insert(Btree* root, int v){
+	if(!root){
+		return getNewNode(v);
+	}
+
+	queue<Btree*> q;
+	q.push(root);
+
+	while(!q.empty()){
+		Btree* temp = q.front();
+		q.pop();
+
+		if(temp -> left){
+			q.push(temp->left);
+		}
+		else {
+			temp -> left = getNewNode(v);
+			break;
+		}
+		if(temp -> right){
+			q.push(temp -> right);
+		}
+		else{
+			temp -> right = getNewNode(v);
+			break;
+		}
+	}
+
+	return root;
+}
+
+void inorder(Btree* root){
+	if(!root)
+		return;
+	inorder(root->left);
+	cout << root->data << " ";
+	inorder(root->right);
+}
+
+int main() {
+	Btree* root = getNewNode(1);
+	root->left = getNewNode(2);
+	root->right = getNewNode(3);
+	root->left->left = getNewNode(4);
+	inorder(root);
+	cout << endl;
+	insert(root, 5);
+	cout << endl;
+	inorder(root);
+	return 0;
+}
