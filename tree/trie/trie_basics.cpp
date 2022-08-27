3c8f3f66f5fc58e468c5090a71923084d93f5daf
Anant Kataria <katariaanant4@gmail.com>
2022-08-27 11:54:52 +0530

    add trie delete recursive function

--- tree/trie/trie_basics.cpp
+++ tree/trie/trie_basics.cpp
@@ -39,9 +39,44 @@ bool search(TrieNode* root, string key){
 	return (pCrawl->isEndOfWord);
 }
 
+bool isEmpty(TrieNode* root){
+	for(int i=0; i<ALPHABET_SIZE; i++)
+		if(root->children[i] != NULL)
+			return false;
+	return true;
+}
+
+TrieNode* remove(TrieNode* root, string key, int depth){
+	if(!root)
+		return NULL;
+
+	if(depth == key.size()) {
+		if(root->isEndOfWord)
+			root->isEndOfWord = false;
+
+		//if given character is not prefix of any other word
+		if(isEmpty(root)){
+			delete(root);
+			root = NULL;
+		}
+
+		return root;
+	}
+
+	int index = key[depth] -  'a';
+	root -> children[index] = remove(root->children[index], key, depth+1);
+
+	if(isEmpty(root) && root->isEndOfWord == false){
+		delete(root);
+		root = NULL;
+	}
+
+	return root;
+}
+
 int main(){
 	string keys[] = {
-		"the", "a", "there", "answer", "any", "by", "bye", "their"
+		"the", "a", "there", "answer", "any", "by", "bye", "their", "hero", "heroplane"
 	};
 	int n = sizeof(keys)/sizeof(keys[0]);
 
@@ -50,10 +85,11 @@ int main(){
 	for(int i=0; i<n; i++)
 		insert(root, keys[i]);
 
-	search(root, "the") ? cout << "Yes\n" : cout << "No\n";
-	search(root, "these") ? cout << "Yes\n" : cout << "No\n";
-	search(root, "their") ? cout << "Yes\n" : cout << "No\n";
-	search(root, "thaw") ? cout << "Yes\n" : cout << "No\n";
+	search(root, "hero") ? cout << "Yes\n" : cout << "No\n";
+	search(root, "heroplane") ? cout << "Yes\n" : cout << "No\n";
+	remove(root, "heroplane", 0);
+	search(root, "hero") ? cout << "Yes\n" : cout << "No\n";
+	search(root, "heroplane") ? cout << "Yes\n" : cout << "No\n";
 
 	return 0;
 }
